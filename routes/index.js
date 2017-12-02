9dfe981ca27bac9e1ef8760a00d415fee96d901a
Ray <raymondyao1997@gmail.com>
2017-12-02 15:25:56 -0800

    add a webpage for record your study

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/register_study.css', function (req,res,next) {
+    res.sendfile('./public/stylesheets/register_study.css');
+});
+
+router.get('/register_study', function(req,res,next){
+  res.sendfile('./views/register_study.html');
+});
+
 module.exports = router;
